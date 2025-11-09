5b580a34b3e25eaffdd402517f20defe12590b22
ShaanDaBest <shaan.mirch@gmail.com>
2025-11-08 20:54:58 -0800

    loadstring

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(HttpGet("https://raw.githubusercontent.com/ShaanDaBest/AeroHub/refs/heads/main/Hub"))()
+loadstring(HttpGet("https://raw.githubusercontent.com/ShaanDaBest/AeroHub/refs/heads/main/aerohub.lua"))()
